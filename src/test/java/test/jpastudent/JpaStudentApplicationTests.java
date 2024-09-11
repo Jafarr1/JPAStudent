4a78a8bd3bb009d3a460c274200f90db2e6f2700
jafar <jaal0002@stud.kea.dk>
2024-09-11 10:26:23 +0200

    java 21 pls virk

--- src/test/java/test/jpastudent/JpaStudentApplicationTests.java
+++ src/test/java/test/jpastudent/JpaStudentApplicationTests.java
@@ -2,14 +2,28 @@ package test.jpastudent;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import test.jpastudent.model.Student;
+import test.jpastudent.repositories.StudentRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JpaStudentApplicationTests {
 
+    @Autowired
+    StudentRepository studentRepository;
     @Test
     void contextLoads() {
         Assertions.assertTrue(true);
     }
 
+    @Test
+    public void Test() {
+        List<Student> lst = studentRepository.findAllByName("Bruce");
+        assertEquals(1, lst.size());
+    }
 }
